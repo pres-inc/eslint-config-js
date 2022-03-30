550a4d1ffa7b00aea715981c0d5546b68fcbd4ad
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-03-31 02:29:09 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# eslint-config-js
+
+## Installation
+
+```bash
+$ npm install -D @pres-inc/eslint-config-js
+```
+
+`.eslintrc.yaml`
+
+```yaml
+extends:
+  - "@pres-inc/eslint-config-js"
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "rules": {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "object-curly-spacing": ["error", "always"],
+    "comma-dangle": ["error", "always-multiline"],
+    "curly": "error",
+    "brace-style": "error",
+    "indent": ["error", 4],
+    "object-shorthand": "error"
+  }
+}
