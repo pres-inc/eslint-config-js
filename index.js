8e55e9251fce578a876560fcadcb65a70ca5de07
kawano-020 <orca.inc0510@gmail.com>
2022-04-19 18:02:37 +0900

    fix style

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 module.exports = {
   "rules": {
-    "semi": ["error", "always"],
     "quotes": ["error", "double"],
     "object-curly-spacing": ["error", "always"],
     "comma-dangle": ["error", "always-multiline"],
     "curly": "error",
     "brace-style": "error",
-    "indent": ["error", 4],
+    "indent": ["error", 2],
     "object-shorthand": "error",
     "no-multi-spaces": "error",
   }
